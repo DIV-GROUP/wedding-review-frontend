348f05f2e30c2e23e62e00d51f0e2db4090c6247
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-06-13 13:19:25 +0000

    pull first

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     <div>
       {/* <Photo /> */}
       {/* <Catering /> */}
-      <Flower />
-      {/* <Makeup /> */}
+      {/* <Flower /> */}
+      <Makeup />
       {/* <Cake /> */}
       {/* <Fashion /> */}
       {/* <Bridal /> */}
